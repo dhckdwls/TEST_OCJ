9b18a136a0cadc6adfa12bab8a69723222db9ed0
dhckdwls <codetama51@gmail.com>
2024-03-17 19:19:49 +0900

    웹개발 기초

---
+++ DB.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS `Spring_AM_01`;
+CREATE DATABASE `Spring_AM_01`;
+USE `Spring_AM_01`;
+
+# article 테이블 생성
+CREATE TABLE article(
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    title CHAR(100) NOT NULL,
+    `body` TEXT NOT NULL
+);
+DROP TABLE IF EXISTS article
+
+SELECT * FROM article
